1d082216d44a892d90ba915e73c940761342aa2d
Nella1a <m.allen@gmx.at>
2022-03-31 23:09:24 +0200

    Add configurations for heroku

--- util/database.js
+++ util/database.js
@@ -6,18 +6,35 @@ import postgres from 'postgres';
 
 config();
 
-// Connect only once to the database
-// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+// Type needed for the connection function below
+declare module globalThis {
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
+}
+
 function connectOneTimeToDatabase() {
-  // When in development, connect only once to the database
-  if (!globalThis.postgresSqlClient) {
-    globalThis.postgresSqlClient = postgres();
+  let sql;
+
+  // production:
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
   }
-  const sql = globalThis.postgresSqlClient;
+  // local environment:
+  else {
+    if (!globalThis.postgresSqlClient) {
+      globalThis.postgresSqlClient = postgres();
+    }
+    sql = globalThis.postgresSqlClient;
+  }
   return sql;
 }
 
+// Connect to PostgresSQl
 const sql = connectOneTimeToDatabase();
+
+
 
 export async function readPlants() {
   const plants = await sql`
