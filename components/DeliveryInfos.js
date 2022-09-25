f4bbf80b15a80196a04a55b3cf5d969760447c60
Nella1a <m.allen@gmx.at>
2022-09-25 21:06:26 +0200

    Add component and css styling

---
+++ components/DeliveryInfos.js
@@ -0,0 +1,11 @@
+import { deliveryInfos } from './elements';
+
+export default function Delivery(){
+  return(
+    <article css={deliveryInfos}>
+     {/* <div>Image</div> */}
+      <p>Free standard delivery on orders over €50</p>
+    </article>
+  );
+
+}
