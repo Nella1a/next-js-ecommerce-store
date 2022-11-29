425c08f52d1dfc1cd866b3f0524caf5cfacec556
Nella1a <m.allen@gmx.at>
2022-11-29 21:24:33 +0100

    Refactor css

--- components/DeliveryInfos.js
+++ components/DeliveryInfos.js
@@ -1,11 +1,17 @@
-import { Fragment } from 'react';
 import { deliveryInfos } from './elements';
 
 export default function Delivery(){
   return(
 
   <section css={deliveryInfos}>
-      <p>Free standard delivery on orders over €50</p>
+    <div>
+      <article>
+      </article>
+      <div>
+        <h4>Free standard delivery on orders over €50</h4>
+        <p>lorem ipsum lorem</p>
+      </div>
+    </div>
     </section>
   );
 
