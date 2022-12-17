1927fbf4d007f0b58afe6973bd09ef22ea216582
Nella1a <m.allen@gmx.at>
2022-12-17 21:32:19 +0100

    Refactor files add typescript

---
+++ pages/thankyou.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Layout from '../components/Layout';
+import { disableGrayLayer } from '../hooks';
+import { PropsTypeGrayLayer } from './types';
+
+export default function ThankYouPage(props: PropsTypeGrayLayer) {
+  disableGrayLayer(props.showGrayLayer, props.setShowGrayLayer)
+  return (
+    <Layout
+    showGrayLayer={props.showGrayLayer}
+    setShowGrayLayer={props.setShowGrayLayer}
+    >
+      <Head>
+        <title>Thank you for your order</title>
+        <meta name="description" content="checkout thank you page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <section css>
+        <h1>Thank you for your order.</h1>
+      </section>
+    </Layout>
+  );
+}
