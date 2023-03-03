10d60e52960ea48aec33205ba011ef49be871e03
Nella1a <m.allen@gmx.at>
2023-03-03 21:54:32 +0100

    Refactor component with imports, typing, and cleanup
    - adds imports for styling and prop typing, as well as a footer component

--- pages/Products.tsx
+++ pages/Products.tsx
@@ -1,34 +1,17 @@
 import { css } from '@emotion/react';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect } from 'react';
 import BackGImage from '../components/BackGImage';
 import Delivery from '../components/DeliveryInfos';
-import {
-  bestSellerStyle,
-  productsComponentStyle,
-} from '../components/elements';
-import Footer from '../components/Footer';
+import { productsComponentStyle } from '../components/elements';
+import Footer from '../components/footer/footer';
 import Layout from '../components/Layout';
 import ProductsComponent from '../components/ProductComponent';
 import { disableGrayLayer } from '../hooks';
 import { readPlants } from '../util/database';
+import { PropsTypePlantsLayer } from './types';
 
-type Plants = {
-  id: number;
-  name: string;
-  price: number;
-};
-
-type Props = {
-  plants: Plants[];
-  showGrayLayer: boolean;
-  setShowGrayLayer: Function;
-};
-
-export default function Products(props: Props) {
+export default function Products(props: PropsTypePlantsLayer) {
 
 disableGrayLayer(props.showGrayLayer, props.setShowGrayLayer)
 
@@ -49,44 +32,17 @@ const bgImageHero = css`
       </Head>
       <BackGImage
 
-//firstText="Delivering Plants,"
-//secondText="Delivering Happiness!"
+// firstText="Delivering Plants,"
+// secondText="Delivering Happiness!"
 bgImage={bgImageHero}
 />
       <section css={productsComponentStyle}>
         <h2>Products</h2>
-
          <article>
             <ProductsComponent plants={props.plants} />
             <ProductsComponent plants={props.plants} />
             <ProductsComponent plants={props.plants} />
          </article>
-
-
-
-
-{/*         {props.plants.map((event) => {
-          return (
-            <div key={`guest-${event.id}`}>
-              <Link href={`/Products/${event.id}`}>
-                <a data-test-id={`data-test-id-${event.id}`}>
-                  {console.log('id:', event.id)}
-                  <Image
-                    src={`/image0${event.id}.jpeg`}
-                    width="393"
-                    height="491,5"
-                    alt="succulenten1"
-                  />
-                </a>
-              </Link>
-              <h2>{event.name}</h2>
-              <p>€{event.price}</p>
-            </div>
-          );
-        })}
- */}
-
-
       </section>
       <Delivery />
       <Footer />
@@ -98,14 +54,12 @@ bgImage={bgImageHero}
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // read plants from database
   const plants = await readPlants();
-  console.log('plants gSSP:', plants);
   // if the cookie is undefined it is going to return an empty array
   // If it is defined it will return everything inside of it
   const cartCookies = context.req.cookies.cart || '[]';
 
   const allCartCookies = JSON.parse(cartCookies);
-  // console.log('headCookdies:', allCartCookies);
-  /* return plants via props to frontend */
+
   return {
     props: {
       plants: plants,
