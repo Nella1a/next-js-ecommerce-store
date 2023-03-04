a7bef1002e29778db638ad0276aa1959fce90a74
Nella1a <m.allen@gmx.at>
2023-03-04 23:21:54 +0100

    Rename components to better reflect their funcitonality

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import BackGImage from './BackGImage';
+
+export function Header(props) {
+  return(<BackGImage
+    firstText="Delivering Plants,"
+    secondText="Delivering Happiness!"
+    bgImageHero={props.bgImageHero}
+    buttonInHeroImage={props.buttonInHeroImage}
+
+  />)
+
+}
