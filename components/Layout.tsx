4273ae7f5cfee67b5217775da02cfbe9533b443c
Nella1a <m.allen@gmx.at>
2023-03-04 23:12:37 +0100

    Refactor layout commoment to include header, footer, deliverinfos  on all pages

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,11 @@
 import Head from 'next/head';
 import { PropsTypeChildrenLayer } from '../pages/types';
-import Header from './Header';
+import Delivery from './DeliveryInfos';
+import Footer from './footer/footer';
+import { Header } from './Header';
+import Navigation from './Navigation';
 
-export default function Layout(props: PropsTypeChildrenLayer) {
+export default function Layout(props: PropsTypeChildrenLayer ) {
 
   return (
     <>
@@ -10,13 +13,23 @@ export default function Layout(props: PropsTypeChildrenLayer) {
         {' '}
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header
+      <Navigation
          showGrayLayer={props.showGrayLayer}
          setShowGrayLayer={props.setShowGrayLayer}
       />
+      <Header
+        firstText="Delivering Plants,"
+        secondText="Delivering Happiness!"
+        bgImageHero={props.bgImageHero}
+    buttonInHeroImage={props.buttonInHeroImage}
+      />
+
       <main>
           {props.children}
+          <Delivery />
       </main>
+      <Footer />
+
     </>
   );
 }
