9f26009c665d40f6c3cfa091add1ffeb73e4f899
Nella1a <m.allen@gmx.at>
2023-03-06 21:55:48 +0100

    Remove unnecessary divs

--- components/DeliveryInfos.tsx
+++ components/DeliveryInfos.tsx
@@ -4,25 +4,20 @@ export default function Delivery(){
   return(
      <section css={deliveryInfos}>
       <div>
+        <div>
+          <article />
+          <p>lorem ipsum lorem</p>
+        </div>
       <div>
         <article />
-        <div>
-        <p>lorem ipsum lorem</p>
-        </div>
+          <p>lorem ipsum lorem ipsum</p>
       </div>
       <div>
         <article />
-        <div>
-          <p>lorem ipsum lorem</p>
-          <p>lorem ipsum lorem</p>
-        </div>
-      </div>
-      <div>
-        <article />
-        <div>
+
         <p>lorem ipsum lorem</p>
-        <p>lorem ipsum lorem</p>
-        </div>
+
+
       </div>
       </div>
     </section>
