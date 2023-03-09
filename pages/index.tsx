af6d076c3938270025defd977e48080f827a9ece
Nella1a <m.allen@gmx.at>
2023-03-09 22:06:46 +0100

    Refactor layout component and add child components

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,29 +10,23 @@ import { readPlants } from '../util/database';
 import { PropsTypePlantsCartCookieLayer } from './types';
 
 const bgImageHero = css`
-  background: no-repeat center url("indexHeroImg.jpeg");
+  background: no-repeat center url('indexHeroImg.jpeg');
   background-size: cover;
 `;
 
-
-
 export default function Home(props: PropsTypePlantsCartCookieLayer) {
-
-
   console.log('Index_props.database typeopf:', typeof props.plants);
   console.log('Index_cartCookie: typeof', typeof props.cartCookies);
-  console.log("Props.RespMenue: ", props.showGrayLayer)
+  console.log('Props.RespMenue: ', props.showGrayLayer);
 
-  const buttonInHeroImage = <ButtonCallToAction innerText="View All Plants" />
-
+  const buttonInHeroImage = <ButtonCallToAction innerText="View All Plants" />;
 
   return (
     <Layout
-    showGrayLayer={props.showGrayLayer}
-    setShowGrayLayer={props.setShowGrayLayer}
-    bgImageHero={bgImageHero}
-    buttonInHeroImage={buttonInHeroImage}
-
+      showGrayLayer={props.showGrayLayer}
+      setShowGrayLayer={props.setShowGrayLayer}
+      bgImageHero={bgImageHero}
+      buttonInHeroImage={buttonInHeroImage}
     >
       <Head>
         <title>Plant Love</title>
@@ -44,13 +38,26 @@ export default function Home(props: PropsTypePlantsCartCookieLayer) {
         <h2>Best Sellers</h2>
         <div>
           <ProductsComponent plants={props.plants} />
+        </div>
+      </section>
+
+      <section css={bestSellerStyle}>
+        <h2>Best Sellers</h2>
+        <div>
           <ProductsComponent plants={props.plants} />
         </div>
       </section>
+
       <section>
         <h2>Summer Favorites</h2>
         <div css={indexTextImageComp}>
           <IndexTextImage plants={props.plants} />
+        </div>
+      </section>
+      <section css={bestSellerStyle}>
+        <h2>On Sale</h2>
+        <div>
+          <ProductsComponent plants={props.plants} />
         </div>
       </section>
     </Layout>
