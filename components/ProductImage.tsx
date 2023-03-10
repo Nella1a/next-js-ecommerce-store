3362d5e773e60d27a7820b2aa0d65a63ba9fc6bb
Nella1a <m.allen@gmx.at>
2023-03-10 22:23:52 +0100

    Refactor images to be responsive - add layout param to image component

--- components/ProductImage.tsx
+++ components/ProductImage.tsx
@@ -1,8 +1,12 @@
+import { css } from '@emotion/react';
 import Image from 'next/image';
 
 type Props = {
   src: string;
 };
+const img = css`
+  max-inline-size: 100%;
+`;
 
 export default function ProductImage(props: Props) {
   return (
@@ -11,6 +15,7 @@ export default function ProductImage(props: Props) {
       width="393"
       height="491,5"
       data-test-id="product-image"
+      layout="responsive"
     />
   );
 }
