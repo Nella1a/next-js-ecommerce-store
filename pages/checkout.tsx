0bae9bdb83837383797b5c324011d994ad7c8204
Nella1a <m.allen@gmx.at>
2023-03-19 18:22:31 +0100

    Add react-hook-form

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,16 +1,44 @@
 import Head from 'next/head';
-import { Form } from 'react-final-form';
+import { useForm } from 'react-hook-form';
 import CheckOutForm from '../components/CheckOutForm';
+import { flexStyle, formStyle } from '../components/elements';
 import LayoutNoHeader from '../components/LayoutNoHeader';
 import { disableGrayLayer } from '../hooks';
 import { PropsTypeGrayLayer } from './types';
+
+interface DefaultFormValues {
+  userMail: string;
+  firstName: string;
+  lastName: string;
+  street: string;
+  country: string;
+  postalCode: string;
+  city: string;
+}
+
+const defaultValues = {
+  userMail: '',
+  firstName: '',
+  lastName: '',
+  street: '',
+  country: '',
+  postalCode: '',
+  city: '',
+};
 
 export default function CheckOut({
   showGrayLayer,
   setShowGrayLayer,
 }: PropsTypeGrayLayer) {
   disableGrayLayer(showGrayLayer, setShowGrayLayer);
-  const onSubmit = () => {};
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setFocus,
+  } = useForm({ defaultValues });
 
   return (
     <LayoutNoHeader
@@ -25,19 +53,26 @@ export default function CheckOut({
       <section>
         {' '}
         <h1>Shipping Information</h1>
-        <Form
-          onSubmit={onSubmit}
-          render={({ handleSubmit, form, submitting, values }) => {
-            return (
-              <CheckOutForm
-                handleSubmit={handleSubmit}
-                form={form}
-                submitting={submitting}
-                values={values}
-              />
-            );
-          }}
-        />
+        <div>
+          <form
+            action="/thankyou"
+            css={formStyle}
+            onSubmit={handleSubmit((values) => {
+              console.log('FormValues: ', values);
+            })}
+          >
+            <CheckOutForm
+              register={register}
+              handleSubmit={handleSubmit}
+              errors={errors}
+              setFocus={setFocus}
+            />
+            <div>
+              <input type="submit" value="Submit" />{' '}
+              <button onClick={() => reset()}>clear fields</button>
+            </div>
+          </form>
+        </div>
       </section>
     </LayoutNoHeader>
   );
