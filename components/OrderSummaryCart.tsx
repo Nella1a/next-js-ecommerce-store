f8bcae8bc7c0df6b171647746dea84f56c5cc2f6
Nella1a <m.allen@gmx.at>
2023-03-20 21:00:32 +0100

    Cleanup

--- components/OrderSummaryCart.tsx
+++ components/OrderSummaryCart.tsx
@@ -1,35 +1,34 @@
 import Link from 'next/link';
 
 type Props = {
-  totalPrice: number
-}
+  totalPrice: number;
+};
 
-export default function OrderSummeryCart(props: Props){
-  return(
+export default function OrderSummaryCart(props: Props) {
+  return (
     <article>
-    <div>
-      <h2>Total</h2>
       <div>
-        <p>
-          <span>Subtotal</span>
-          <span>{props.totalPrice}</span>
-        </p>
-        <p>
-          <span>Delivery</span>
-          <span>€ 0.00</span>
-        </p>
+        <h2>Total</h2>
+        <div>
+          <p>
+            <span>Subtotal</span>
+            <span>{props.totalPrice}</span>
+          </p>
+          <p>
+            <span>Delivery</span>
+            <span>€ 0.00</span>
+          </p>
+        </div>
+        <div>
+          <p>
+            <span>Total (VAT included)</span>
+            <span>€ {props.totalPrice}</span>
+          </p>
+          <Link href="/checkout" passHref>
+            <button data-test-id="cart-checkout">Go to checkout</button>
+          </Link>
+        </div>
       </div>
-      <div>
-      <p>
-        <span>Total (VAT included)</span>
-        <span>€ {props.totalPrice}</span>
-      </p>
-      <Link href="/checkout" passHref>
-        <button data-test-id="cart-checkout">Go to checkout</button>
-      </Link>
-      </div>
-    </div>
-  </article>
-  )
-
-  }
+    </article>
+  );
+}
