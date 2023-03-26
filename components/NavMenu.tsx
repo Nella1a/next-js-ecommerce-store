70924fec8b0fc37876a33d73693d76061e864347
Nella1a <m.allen@gmx.at>
2023-03-26 17:14:43 +0200

    Cleanup - remove comments

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -2,20 +2,6 @@ import Link from 'next/link';
 
 export default function NavMenu() {
   return (
-    /*    <>
-      <Link href="/products">
-        <a data-test-id="products-link">Plants</a>
-      </Link>
-      <Link href="/inspiration">
-        <a>Inspiration</a>
-      </Link>
-      <Link href="/sale">
-        <a>Sale</a>
-      </Link>
-      <Link href="/underconstruction">
-        <a>Contact</a>
-      </Link>
-    </> */
     <>
       <li>
         <Link href="/products">
