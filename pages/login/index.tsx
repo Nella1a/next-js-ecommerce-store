d8d235447a963092b3501c199c42cf325d0b8e3b
Nella1a <m.allen@gmx.at>
2023-10-11 20:24:14 +0200

    Add draft for longin confirmation page and refactor navbar login link

---
+++ pages/login/index.tsx
@@ -0,0 +1,11 @@
+import LayoutNoHeader from '../../components/Layout/LayoutNoHeader';
+
+export default function LogInHome() {
+  return (
+    <LayoutNoHeader>
+      <section>
+        <h1>YES YOU ARE LOGGED IN</h1>
+      </section>
+    </LayoutNoHeader>
+  );
+}
