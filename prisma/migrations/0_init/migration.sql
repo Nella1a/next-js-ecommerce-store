e6dd3c13e9687d96e38f2bb73d86ee3a9a5a9ea6
Nella1a <m.allen@gmx.at>
2023-10-14 23:31:00 +0200

    Add - config db prisma connection, add prisma schema, create baseline migration

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,194 @@
+-- CreateTable
+CREATE TABLE "cart_items" (
+    "id" SERIAL NOT NULL,
+    "product_id" INTEGER NOT NULL,
+    "quantity" INTEGER,
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "cart_items_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "carts" (
+    "id" SERIAL NOT NULL,
+    "session_id" INTEGER,
+    "created_at" TIMESTAMPTZ(6) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "carts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "carts_and_items" (
+    "cart_id" INTEGER NOT NULL,
+    "cart_item_id" INTEGER NOT NULL,
+
+    CONSTRAINT "carts_and_items_pk" PRIMARY KEY ("cart_id","cart_item_id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(60) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "migrations" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "migrations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "order_status" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(60) NOT NULL,
+
+    CONSTRAINT "order_status_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "quantity" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "total_price" DECIMAL(10,2) NOT NULL,
+    "status_id" INTEGER,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "product_categories" (
+    "product_id" INTEGER NOT NULL,
+    "category_id" INTEGER NOT NULL,
+
+    CONSTRAINT "product_categories_pk" PRIMARY KEY ("product_id","category_id")
+);
+
+-- CreateTable
+CREATE TABLE "product_orders" (
+    "product_id" INTEGER NOT NULL,
+    "order_id" INTEGER NOT NULL,
+
+    CONSTRAINT "product_orders_pk" PRIMARY KEY ("product_id","order_id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(50) NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "descr" TEXT NOT NULL,
+    "slug" VARCHAR(50),
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "shipping_address" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER,
+    "address_line" VARCHAR(255),
+    "city" VARCHAR(255),
+    "zip_code" VARCHAR(255),
+    "country" VARCHAR(255),
+    "company" VARCHAR(255),
+
+    CONSTRAINT "shipping_address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_roles" (
+    "id" SERIAL NOT NULL,
+    "role_name" VARCHAR(30) NOT NULL,
+
+    CONSTRAINT "user_roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_sessions" (
+    "id" SERIAL NOT NULL,
+    "token" VARCHAR(90) NOT NULL,
+    "expiry_timestamp" TIMESTAMP(6) NOT NULL DEFAULT (now() + '12:00:00'::interval),
+    "user_id" INTEGER,
+
+    CONSTRAINT "user_sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" VARCHAR(30) NOT NULL,
+    "password_hash" VARCHAR(60) NOT NULL,
+    "username" VARCHAR(30),
+    "first_name" VARCHAR(50),
+    "last_name" VARCHAR(50),
+    "role_id" INTEGER,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users_payment" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "card_type_id" INTEGER,
+    "cardnumber" INTEGER,
+    "date_expire" DATE,
+
+    CONSTRAINT "users_payment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_sessions_token_key" ON "user_sessions"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "cart_items" ADD CONSTRAINT "cart_items_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "carts" ADD CONSTRAINT "carts_session_id_fkey" FOREIGN KEY ("session_id") REFERENCES "user_sessions"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "carts_and_items" ADD CONSTRAINT "carts_and_items_cart_id_fkey" FOREIGN KEY ("cart_id") REFERENCES "carts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "carts_and_items" ADD CONSTRAINT "carts_and_items_cart_item_id_fkey" FOREIGN KEY ("cart_item_id") REFERENCES "cart_items"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "order_status"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "product_categories" ADD CONSTRAINT "product_categories_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "product_categories" ADD CONSTRAINT "product_categories_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "product_orders" ADD CONSTRAINT "product_orders_order_id_fkey" FOREIGN KEY ("order_id") REFERENCES "orders"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "product_orders" ADD CONSTRAINT "product_orders_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "products"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "shipping_address" ADD CONSTRAINT "shipping_address_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "user_sessions" ADD CONSTRAINT "user_sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "user_roles"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users_payment" ADD CONSTRAINT "users_payment_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
