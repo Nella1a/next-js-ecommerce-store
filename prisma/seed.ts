05db037c9a6724dc460f02107f31c55aa3783f24
Nella1a <m.allen@gmx.at>
2023-10-15 12:46:41 +0200

    Add initial product data to db

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+const products = [
+  {
+    title: 'Monstera Deliciosa',
+    price: 17.95,
+    descr:
+      'Monstera are species of evergreen tropical vines and shrubs that are native to Central America. They are famous for their natural leaf-holes, which has led to the rise of their nickname, Swiss Cheese Plant.',
+  },
+  {
+    title: 'Ficus Lyrata',
+    price: 9.95,
+    descr:
+      'Needs Bright, Filtered Light. Will rapidly deteriorate in dim lighting.Keep soil evenly moist at all times. (about 2-3x per week)  Do not overwater or leave plant sitting in water.',
+  },
+  {
+    title: 'Pilea Peperomioides',
+    price: 12.95,
+    descr:
+      'An easy plant to care for. Like most of our favorite indoor house plants, Pilea prefers bright, indirect light. Too much direct sun will scorch the round leaves.Water about once a week, or when the soil is nearly dry. Keep your plant in a well draining pot to avoid letting the roots sit in water.',
+  },
+  {
+    title: 'Calathea Orbifolia',
+    price: 26.95,
+    descr:
+      'The Calathea orbifolia is a flowering species of plant in the family Marantaceae. The perennial plants are native to tropical forests in Central and South America, Asia, and Africa. Outdoors, calatheas grow on the forest floor, where they thrive in shaded, warm, humid environments.',
+  },
+];
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.products.createMany({ data: products });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+const emptyModuleExport = {};
+export default emptyModuleExport;
