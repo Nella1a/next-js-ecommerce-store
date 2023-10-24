02c4ce27d559c991ec7267a8e43bff1e84f0b828
Nella1a <m.allen@gmx.at>
2023-10-24 19:40:20 +0200

    Add toggle for loginForm layover

--- util/context/overlayContext.tsx
+++ util/context/overlayContext.tsx
@@ -3,18 +3,33 @@ import { Children, createContext, useEffect, useState } from 'react';
 export const OverlayContext = createContext({
   toggle: false,
   toggleLayover: () => {},
+  toggleLogin: false,
+  toggleLoginLayover: () => {},
 });
 
 export const OverLayContextProvider = ({ children }: any) => {
   const [toggle, setToggle] = useState(false);
+  const [toggleLogin, setToggleLogin] = useState(false);
 
   const toggleLayover = () => {
+    if (toggleLogin) {
+      setToggleLogin(false);
+    }
     setToggle(!toggle);
   };
-  console.log('toggle2: ', toggle);
+
+  const toggleLoginLayover = () => {
+    if (toggle) {
+      setToggle(false);
+    }
+    setToggleLogin(!toggleLogin);
+  };
+
   const value = {
     toggle,
     toggleLayover,
+    toggleLogin,
+    toggleLoginLayover,
   };
   return (
     <OverlayContext.Provider value={value}>{children}</OverlayContext.Provider>
