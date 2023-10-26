6baccaee797aaa970dd0441b907d2c84e16eb51f
Nella1a <m.allen@gmx.at>
2023-10-26 12:59:56 +0200

    Add new page 'account'

---
+++ pages/account/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout';
+
+export default function Account() {
+  return (
+    <>
+      <Layout>
+        <h1>Your SheLovesPlants Konto </h1>
+      </Layout>
+    </>
+  );
+}
