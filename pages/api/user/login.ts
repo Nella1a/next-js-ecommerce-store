729e957fe2ebdabdfe958a1c251971dc8a1dd865
Nella1a <m.allen@gmx.at>
2023-10-29 16:37:30 +0100

    Refactor user  authentication and api endpoints 'login and register' and remove nextAuth config

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -3,11 +3,12 @@ import { Prisma } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../prisma';
+import { verifyCsrfToken } from '../../../util/auth';
 
 type ResponseData = {
   id: number;
-  username: string;
-  email: string;
+  username?: string;
+  email?: string;
 };
 
 export type Error = { errors: { message: string }[] };
@@ -17,15 +18,31 @@ export default async function handler(
   res: NextApiResponse<ResponseData | Error>,
 ) {
   if (req.method === 'POST') {
-    console.log('request.body', req.body);
-    const { email, password } = req.body;
-
-    // validation: fields are not empty
-    if (!email || !password) {
+    console.log('request.body login', req.body);
+    const { email, password, csrfToken } = req.body;
+    if (
+      typeof password !== 'string' ||
+      !password ||
+      typeof password !== 'string' ||
+      !password ||
+      typeof csrfToken !== 'string' ||
+      !csrfToken ||
+      typeof email !== 'string' ||
+      !email
+    ) {
       res.status(400).json({
+        errors: [{ message: 'email, password or CSRF token not provided' }],
+      });
+      return;
+    }
+
+    // Verify CSRF Token
+    const csrfTokenMatches = verifyCsrfToken(csrfToken);
+    if (!csrfTokenMatches) {
+      res.status(403).json({
         errors: [
           {
-            message: 'Invalid inputs',
+            message: 'Invalid CSRF token',
           },
         ],
       });
@@ -33,45 +50,77 @@ export default async function handler(
     }
 
     // query user in db
-    try {
-      const user = await prisma.user.findUnique({
-        where: {
-          email: email,
-        },
-        select: {
-          id: true,
-          email: true,
-          password_hash: true,
-          username: true,
-        },
-      });
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+      select: {
+        id: true,
+        email: true,
+        password_hash: true,
+        username: true,
+      },
+    });
 
-      // check password
-      if (user) {
-        const passwordHash = await bcrypt.compare(user.password_hash, password);
-        if (passwordHash) {
-          return res.status(201).json({
-            id: user.id,
-            username: user.username,
-            email: user.email,
-          });
-        }
-      } else {
-        return res
-          .status(401)
-          .json({ errors: [{ message: 'Invalid credentials' }] });
-      }
-    } catch (e) {
-      if (e instanceof Prisma.PrismaClientKnownRequestError) {
-        if (e.code === 'P2002') {
-          return res.status(400).json({ errors: [{ message: e.message }] });
-        }
-        return res.status(400).json({ errors: [{ message: e.message }] });
-      }
+    console.log('found user: ', user);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ errors: [{ message: 'Invalid credentials' }] });
     }
+
+    // compare passwordHash
+    const passwordMatches = await bcrypt.compare(password, user.password_hash);
+
+    console.log('passwordHast: ', passwordMatches);
+    if (!passwordMatches) {
+      console.log('PW is not right');
+      return res
+        .status(401)
+        .json({ errors: [{ message: 'Invalid credentials' }] });
+    }
+
+    // 1. Create a unique token (use node crypto)
+    const sessionToken = crypto.randomBytes(64).toString('base64');
+
+    const session = await prisma.userSession.create({
+      data: {
+        token: sessionToken,
+        user_id: user.id,
+      },
+    });
+
+    // todo:   3. Serialize the Cookie?
+
+    res.status(201).setHeader('Set-Cookie', sessionToken).json({
+      id: user.id,
+    });
+    return;
   } else {
-    res.status(405).json({
+    return res.status(405).json({
       errors: [{ message: 'Method not supported, try POST instead' }],
     });
   }
 }
+
+//  // query user in db
+//  try {
+//   const user = await prisma.user.findUnique({
+//     where: {
+//       email: email,
+//     },
+//     select: {
+//       id: true,
+//       email: true,
+//       password_hash: true,
+//       username: true,
+//     },
+//   });
+// } catch (e) {
+//   if (e instanceof Prisma.PrismaClientKnownRequestError) {
+//     if (e.code === 'P2002') {
+//       return res.status(400).json({ errors: [{ message: e.message }] });
+//     }
+//     return res.status(400).json({ errors: [{ message: e.message }] });
+//   }
+// }
--- util/context/overlayContext.tsx
+++ util/context/overlayContext.tsx
@@ -1,35 +1,20 @@
-import { Children, createContext, useEffect, useState } from 'react';
+import { Children, createContext, useState } from 'react';
 
 export const OverlayContext = createContext({
   toggle: false,
   toggleLayover: () => {},
-  toggleLogin: false,
-  toggleLoginLayover: () => {},
 });
 
 export const OverLayContextProvider = ({ children }: any) => {
   const [toggle, setToggle] = useState(false);
-  const [toggleLogin, setToggleLogin] = useState(false);
 
   const toggleLayover = () => {
-    if (toggleLogin) {
-      setToggleLogin(false);
-    }
     setToggle(!toggle);
-  };
-
-  const toggleLoginLayover = () => {
-    if (toggle) {
-      setToggle(false);
-    }
-    setToggleLogin(!toggleLogin);
   };
 
   const value = {
     toggle,
     toggleLayover,
-    toggleLogin,
-    toggleLoginLayover,
   };
   return (
     <OverlayContext.Provider value={value}>{children}</OverlayContext.Provider>
