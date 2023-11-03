82f74aca1bc23f43e51c3724dd737a33f8f4e48f
Nella1a <m.allen@gmx.at>
2023-11-03 23:07:56 +0100

    Add login and register page and refactor page options in nextAuth config

---
+++ pages/register.tsx
@@ -0,0 +1,57 @@
+import { css } from '@emotion/react';
+import { GetServerSidePropsContext } from 'next';
+import { getServerSession } from 'next-auth/next';
+import { getSession, useSession } from 'next-auth/react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Router, { useRouter } from 'next/router';
+import LayoutNoHeader from '../components/Layout/LayoutNoHeader';
+import RegisterForm from '../components/RegisterForm';
+import prisma from '../prisma';
+import { createCsrfToken } from '../util/auth';
+import { authOptions } from './api/auth/[...nextauth]';
+
+const accountStyle = css`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  border: 1px solid red;
+`;
+
+type Props = {
+  csrfToken: string;
+  session?: any;
+  test: string;
+};
+
+export default function register(props: Props) {
+  const { data, status } = useSession();
+  console.log('props.session: ', props.session);
+  console.log('props.test: ', props);
+
+  //const { user, status } = props.session;
+  const router = useRouter();
+  ///console.log('data: ', user);
+  console.log('token: ', props.csrfToken);
+  if (status === 'authenticated') Router.push('/');
+  if (status !== 'authenticated') {
+    return (
+      <>
+        <LayoutNoHeader>
+          <Head>
+            <title>Account</title>
+            <meta name="description" content="Plant Shop" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <section css={accountStyle}>
+            <RegisterForm token={props.csrfToken} />
+          </section>
+          <section>
+            <p>Already have an account?</p>
+            <Link href={'/signIn'}>Login here!</Link>
+          </section>
+        </LayoutNoHeader>
+      </>
+    );
+  }
+}
