f6b38fddf26daaa1af3c04db1f49c598a1cbbaa7
Nella1a <m.allen@gmx.at>
2023-11-04 23:01:56 +0100

    Add logic to display or hide login-form layover

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import { SessionProvider } from 'next-auth/react';
+import { useContext } from 'react';
 import { globalStyleBody } from '../components/elements';
+import LoginInFormLayover from '../components/LoginFormLayover';
 import theme from '../components/theme';
 import { CartContextProvider } from '../util/context/cartContext';
 import { CartCookieProvider } from '../util/context/cookieContext';
-import { OverLayContextProvider } from '../util/context/overlayContext';
+import {
+  OverlayContext,
+  OverLayContextProvider,
+} from '../util/context/overlayContext';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
+  const { loginLayover } = useContext(OverlayContext);
+  console.log('loginLayover: ', loginLayover);
   return (
     <>
       <SessionProvider session={session}>
@@ -16,6 +23,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
               <ThemeProvider theme={theme}>
                 <Global styles={globalStyleBody(theme)} />
                 <Component {...pageProps} />
+                <LoginInFormLayover token={''} />
               </ThemeProvider>
             </CartCookieProvider>
           </CartContextProvider>
--- util/context/overlayContext.tsx
+++ util/context/overlayContext.tsx
@@ -3,18 +3,27 @@ import { Children, createContext, useState } from 'react';
 export const OverlayContext = createContext({
   toggle: false,
   toggleLayover: () => {},
+  loginLayover: false,
+  toggleLoginLayover: () => {},
 });
 
 export const OverLayContextProvider = ({ children }: any) => {
   const [toggle, setToggle] = useState(false);
 
+  const [loginLayover, setLoginLayover] = useState(false);
+
   const toggleLayover = () => {
     setToggle(!toggle);
   };
 
+  const toggleLoginLayover = () => {
+    setLoginLayover(!loginLayover);
+  };
   const value = {
     toggle,
     toggleLayover,
+    loginLayover,
+    toggleLoginLayover,
   };
   return (
     <OverlayContext.Provider value={value}>{children}</OverlayContext.Provider>
