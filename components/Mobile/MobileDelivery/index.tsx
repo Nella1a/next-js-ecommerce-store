20944f51b5cb3917fc085a4424f9d8ab7ac101b1
Nella1a <m.allen@gmx.at>
2023-11-09 20:45:43 +0100

    Add new component -  delivery infos without image

---
+++ components/Mobile/MobileDelivery/index.tsx
@@ -0,0 +1,19 @@
+import { deliveryInfos } from '../../elements';
+
+export default function MobileDelivery() {
+  return (
+    <section css={deliveryInfos}>
+      <div>
+        <div>
+          <p>Free Shipping</p>
+        </div>
+        <div>
+          <p>30 Day Guarantee</p>
+        </div>
+        <div>
+          <p>Best Quality</p>
+        </div>
+      </div>
+    </section>
+  );
+}
