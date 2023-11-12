18ce6762a51e327034897e3cb5c1735d01fc4758
Nella1a <m.allen@gmx.at>
2023-11-12 23:28:46 +0100

    Cleanup remove unused import

--- pages/thankyou.tsx
+++ pages/thankyou.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
-import { disableGrayLayer } from '../hooks';
 
 export default function ThankYouPage() {
   // disableGrayLayer(props.showGrayLayer, props.setShowGrayLayer);
