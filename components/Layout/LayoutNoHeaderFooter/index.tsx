376ac99124e7f5a26f62e0c05f299e0c4ffc4e09
Nella1a <m.allen@gmx.at>
2023-11-13 22:04:57 +0100

    Cleanup remove unused imports

--- components/Layout/LayoutNoHeaderFooter/index.tsx
+++ components/Layout/LayoutNoHeaderFooter/index.tsx
@@ -1,10 +1,5 @@
 import Head from 'next/head';
 import { PropsLayoutCart } from '../../../util/types';
-import Delivery from '../../Delivery';
-import Footer from '../../Footer';
-import MobileDelivery from '../../Mobile/MobileDelivery';
-import MobileFooter from '../../Mobile/MobileFooter';
-import Navigation, { BREAKPOINTAT768, getScreenSize } from '../../Navigation';
 import NavWithLogoOnly from '../../Navigation/NavWithLogoOnly';
 
 export default function LayoutNoHeaderAndFooter(props: PropsLayoutCart) {
