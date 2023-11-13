21d4f3408fb6ea5bea386d5a3470a66169405953
Nella1a <m.allen@gmx.at>
2023-11-13 21:56:18 +0100

    Refactor the navigation bar by adding a container for centering and fix z-index

--- components/Navigation/NavWithLogoOnly/index.tsx
+++ components/Navigation/NavWithLogoOnly/index.tsx
@@ -3,41 +3,48 @@ import Link from 'next/link';
 import { Fragment } from 'react';
 
 export const checkOutHeaderStyle = css`
-  max-width: 1920px;
-  width: 100%;
-  position: fixed;
-  background-color: var(--backgroundColorWhite);
-  // background-color: yellow;
+  position: sticky;
   top: 0;
-  z-index: 1;
-  margin: auto;
-  display: flex;
-  padding: 0 3rem;
-  padding-top: 0.5rem;
-  border-bottom: 0px solid lightgrey;
-  height: 6rem;
+  background-color: transparent;
+  width: 100vw;
+  border-bottom: 1px solid lightgrey;
+  // border: 2px solid red;
 
-  // shelovesPlants logo
-  > div:first-of-type {
+  // container
+  > div {
+    max-width: 1920px;
     width: 100%;
-    margin: 0 auto;
-    position: relative;
-    top: 0px;
-    gap: 2rem;
-    align-items: center;
-    display: flex;
-    justify-content: space-between;
-    z-index: 1;
-    height: 4rem;
-    border-bottom: 1px solid lightgray;
+    background-color: var(--backgroundColorWhite);
 
-    img {
-      width: 80%;
-      height: auto;
+    top: 0;
+    z-index: 1;
+    margin: auto;
+    display: flex;
+    padding: 0 3rem;
+    padding-top: 0.5rem;
+    height: 6rem;
+
+    // shelovesPlants logo
+    > div:first-of-type {
+      width: 100%;
+      margin: 0 auto;
+
+      top: 0px;
+      gap: 2rem;
+      align-items: center;
+      display: flex;
+      justify-content: space-between;
+      z-index: 1;
+      height: 4rem;
+
+      img {
+        width: 80%;
+        height: auto;
+      }
     }
-  }
-  // links
-  > div:nth-of-type(2) {
+    // links
+    > div:nth-of-type(2) {
+    }
   }
 `;
 
@@ -46,11 +53,13 @@ export default function NavWithLogoOnly() {
     <Fragment>
       <nav css={checkOutHeaderStyle}>
         <div>
-          <Link href="/" passHref>
-            <img src="/logo_shelovesplants.svg" alt="logo she loves plants" />
-          </Link>
+          <div>
+            <Link href="/" passHref>
+              <img src="/logo_shelovesplants.svg" alt="logo she loves plants" />
+            </Link>
+          </div>
+          <div></div>
         </div>
-        <div></div>
       </nav>
     </Fragment>
   );
