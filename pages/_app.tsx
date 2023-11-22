6a94e06d34b2557577ecd1d029bcfb8646381400
Nella1a <m.allen@gmx.at>
2023-11-22 08:28:08 +0100

    Cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
 import { Global, ThemeProvider } from '@emotion/react';
 import { SessionProvider } from 'next-auth/react';
-import { useContext } from 'react';
 import { globalStyleBody } from '../components/elements';
 import LoginInFormLayover from '../components/LoginFormLayover';
 import theme from '../components/theme';
 import { CartContextProvider } from '../util/context/cartContext';
 import { CartCookieProvider } from '../util/context/cookieContext';
-import {
-  OverlayContext,
-  OverLayContextProvider,
-} from '../util/context/overlayContext';
+import { OverLayContextProvider } from '../util/context/overlayContext';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
-  const { loginLayover } = useContext(OverlayContext);
-  console.log('loginLayover: ', loginLayover);
   return (
     <>
       <SessionProvider session={session}>
