43f1e7b0f5be1d510688044587a63efe906bca9b
Nella1a <m.allen@gmx.at>
2024-02-11 18:22:24 +0100

    Refactor e2e navigation test

--- integration/navigation.spec.ts
+++ integration/navigation.spec.ts
@@ -1,21 +1,48 @@
+// @ts-check
 import { expect, test } from '@playwright/test';
 
-const plants = [];
-
-test('should navigate to the products page', async ({ page }) => {
-  // start form the index page
+test('test navigation links', async ({ page }) => {
+  // start from the index page
   await page.goto('/');
 
-  // get link
-  const plantsLink = page.getByRole('link', { name: 'Plants' });
-  await plantsLink.click();
+  await expect(
+    page.getByRole('link', { name: 'Plants', exact: true }),
+  ).toContainText('Plants');
 
-  //find element with the text Plants and click on it
-  // await page.click('text=Plants');
+  // navigate to products overview page
+  await page.getByRole('link', { name: 'Plants', exact: true }).click();
 
   // the new url should be "/products"
   await expect(page).toHaveURL('/products');
 
   // the new page should contain an h1 with "Products"
-  await expect(page.locator('h1')).toContainText('Products');
+  await expect(
+    page.getByRole('heading', { name: 'Products', exact: true }),
+  ).toBeVisible();
+
+  // navigate to cart
+  await page.getByAltText('shopping cart icon').click();
+  await expect(page.getByText('Your cart is currently empty.')).toBeVisible();
+  await expect(
+    page.getByRole('button', { name: 'CONTINUE SHOPPING' }),
+  ).toBeVisible();
+
+  // click logo to navigate back to index
+  await page.getByAltText('logo she loves plants').click();
+
+  await expect(
+    page.getByRole('button', { name: 'VIEW ALL PLANTS' }),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('heading').filter({ hasText: 'On Sale' }),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('heading').filter({ hasText: 'Best Sellers' }).first(),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('heading').filter({ hasText: 'Summer Favorites' }),
+  ).toBeVisible();
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -24,10 +24,11 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:3001',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    testIdAttribute: 'data-testid',
   },
 
   /* Configure projects for major browsers */
@@ -37,7 +38,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    /*     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -45,7 +46,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    }, */
 
     /* Test against mobile viewports. */
     // {
