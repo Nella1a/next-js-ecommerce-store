5941fd06e53cfea89f7b797a21200067ce100728
Nella1a <m.allen@gmx.at>
2024-02-16 23:24:34 +0100

    Refactor href

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -45,7 +45,7 @@ export default function Navigation() {
     <button css={shoppingBagStyle}>
       <Link
         href={{
-          pathname: '/cart',
+          pathname: '/Cart',
         }}
         passHref
       >
