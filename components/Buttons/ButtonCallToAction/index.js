c9569c5c3933faac60a560299d9414542111315f
Nella1a <m.allen@gmx.at>
2024-03-10 23:26:55 +0100

    Cleanup

--- components/Buttons/ButtonCallToAction/index.js
+++ components/Buttons/ButtonCallToAction/index.js
@@ -3,9 +3,10 @@ import Link from 'next/link';
 export default function ButtonCallToAction(props) {
   return (
     <Link
-      href={{
-        pathname: '/plants',
-      }}
+      // href={{
+      //   pathname: '/plants',
+      // }}
+      href="#"
       passHref
     >
       <button data-test-id="button-view-all-plants" css={props.styleb}>
