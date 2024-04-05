108fe5cf277148cd24a5eb3fb5b90721167e44e3
Nella1a <m.allen@gmx.at>
2024-04-05 18:53:57 +0200

    Refactor test to test checkout process

--- tests/checkoutProcess.spec.ts
+++ tests/checkoutProcess.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('test add product to cart and got to checkout page', async ({ page }) => {
+test('test checkout process', async ({ page }) => {
   // start from the index page
   await page.goto('/');
 
@@ -19,7 +19,7 @@ test('test add product to cart and got to checkout page', async ({ page }) => {
   await expect(page.getByRole('button', { name: '+' })).toBeVisible();
   await expect(page.getByTestId('add-quantity')).toContainText('1');
 
-  //add quantity to cart
+  //add to cart
   await page.getByRole('button', { name: '+' }).click();
   await page.getByRole('button', { name: '+' }).click();
   await expect(page.getByTestId('add-quantity')).toContainText('3');
@@ -43,7 +43,47 @@ test('test add product to cart and got to checkout page', async ({ page }) => {
   await expect(page.getByText('Subtotal')).toBeInViewport();
   await expect(page.getByText('Delivery', { exact: true })).toBeInViewport();
 
-  // checkout button
+  // navigate to checkout page
   await page.getByRole('link', { name: 'Go to checkout' }).click();
   await expect(page).toHaveURL('/checkout');
+
+  // fill out contact information
+  await expect(
+    page.getByRole('heading', { name: 'Contact information', exact: true }),
+  ).toBeVisible();
+  //  await page.getByPlaceholder('Email').fill('testemail@test.at');
+  await page.getByRole('textbox', { name: 'Email' }).fill('testemail@test.at');
+
+  // fill out shipping details
+  await expect(
+    page.getByRole('heading', { name: 'Shipping address', exact: true }),
+  ).toBeVisible();
+  await page.getByRole('textbox', { name: 'First name' }).fill('First');
+  await page.getByRole('textbox', { name: 'Last name' }).fill('Name');
+  await page.getByRole('textbox', { name: 'Address' }).fill('Test Street 1');
+  await page.getByRole('textbox', { name: 'City' }).fill('Vienna');
+  await page.getByRole('textbox', { name: 'Zip Code' }).fill('1030');
+  await page.getByRole('textbox', { name: 'Country' }).fill('Austria');
+
+  await page.getByRole('button', { name: 'Continue to Payment' }).click();
+
+  // fill out payment details
+  await page.getByRole('combobox').selectOption('visa');
+  await expect(
+    page.getByRole('heading', { name: 'Payment information', exact: true }),
+  ).toBeVisible();
+
+  await page.getByRole('textbox', { name: 'Card number' }).fill('123123123123');
+  await page.getByRole('textbox', { name: 'Name on card' }).fill('First Name');
+  await page
+    .getByRole('textbox', { name: 'Expiry Date (MM/YY)' })
+    .fill('07/25');
+  await page.getByRole('textbox', { name: 'Security Code (CVC)' }).fill('123');
+
+  // place order and navigate to thank you page
+  await page.getByRole('button', { name: 'Place order' }).click();
+  await expect(page).toHaveURL('/thankyou');
+  await expect(
+    page.getByRole('heading', { name: 'Thank you for your order' }),
+  ).toBeVisible();
 });
