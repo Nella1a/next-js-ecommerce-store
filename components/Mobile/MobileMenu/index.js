68e052a7a609a5ef6ecab67ac1cc33c0e8f75fa0
Nella1a <m.allen@gmx.at>
2024-04-08 22:50:35 +0200

    Refactor - Convert SVG attributes to camelCase format

--- components/Mobile/MobileMenu/index.js
+++ components/Mobile/MobileMenu/index.js
@@ -123,13 +123,13 @@ export default function MobileMenu() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
-                class="menuCloseIcon"
+                className="menuCloseIcon"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M6 18 18 6M6 6l12 12"
                 />
               </svg>
