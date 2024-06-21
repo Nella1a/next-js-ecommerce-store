b17397d203992f5ccd768565a1e79397db2aee5b
Nella1a <m.allen@gmx.at>
2024-06-21 14:45:50 +0200

    Refactor - total price calculation, cleanup

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -1,3 +1,4 @@
+import { Prisma, PrismaClient } from '@prisma/client';
 import { getCookie } from 'cookies-next';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../prisma';
@@ -49,76 +50,66 @@ export default async function handler(
       const paymentInfos = contactDetails.payment;
       //toDo form payment validation
 
-      // update db:
+      // create payment id:
       const openPayment = await prisma.orderPayments.create({
         data: {
           status_id: 1,
         },
       });
 
-      // items
+      // get cart items
       const cartItems = await prisma.cartItem.findMany({
         where: { user_id: user.id },
         select: {
           product_id: true,
           quantity: true,
+          product: {
+            select: {
+              price: true,
+            },
+          },
         },
       });
 
-      let totalPrice = 0;
-      cartItems.map(async (item) => {
-        const price = await prisma.product.findUnique({
-          where: {
-            id: item.product_id,
-          },
-          select: {
-            price: true,
-          },
-        });
-        console.log('----,> Price: ', price);
-
-        totalPrice = totalPrice * item.quantity + Number(price?.price);
-        console.log('----,>total Price: ', totalPrice);
+      // calculate total price
+      const cartQuantityAndPrice = cartItems.map((items) => {
+        return { ...items, p: items.quantity * Number(items.product.price) };
       });
 
-      console.log('PRICE: ', totalPrice);
+      const totalPrice = cartQuantityAndPrice.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.p,
+        0,
+      );
+
+      // create new order
       const newOrder = await prisma.order.create({
         data: {
-          total_price: totalPrice,
+          total_price: new Prisma.Decimal(totalPrice),
           status_id: 1,
           user_id: user.id,
           payment_id: openPayment.id,
         },
       });
 
+      // add items to user order
       cartItems.map(async (item) => {
-        const product = await prisma.product.findUnique({
-          where: {
-            id: item.product_id,
-          },
-          select: {
-            id: true,
+        await prisma.orderItem.create({
+          data: {
+            quantity: item.quantity,
+            product_id: item.product_id,
+            order_id: newOrder.id,
           },
         });
-
-        if (product) {
-          await prisma.orderItem.create({
-            data: {
-              quantity: item.quantity,
-              product_id: product.id,
-              order_id: newOrder.id,
-            },
-          });
-        }
       });
 
-      // delete cartItems:
+      // delete items from user cart
       await prisma.cartItem.deleteMany({
         where: {
           user_id: user.id,
         },
       });
 
+      // get order history
       const orderHistory = await prisma.orderItem.findMany({
         where: {
           order: {
@@ -150,10 +141,6 @@ export default async function handler(
         },
       });
 
-      console.log(
-        '-xxxxx---> orderHistory: ',
-        orderHistory[0].order.total_price,
-      );
       res.status(200).json(orderHistory);
       return;
     } else {
