c248866e928c1928d88d5fb61ca23317a2ec35ab
Nella1a <m.allen@gmx.at>
2024-07-03 07:35:45 +0200

    Refactor add 'add to cart' logic

--- pages/api/addToCart.ts
+++ pages/api/addToCart.ts
@@ -1,16 +1,98 @@
+import { getCookie } from 'cookies-next';
 import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../prisma';
+import { firebaseAdmin } from '../../util/firebase-admin-config';
 
 export type AddToCartResponseBody =
-  | { errors: { message: string } }
-  | { cart: { id: number; quantity: number }[] | undefined };
+  | { error: { message: string } }
+  | { id: number; quantity: number }
+  | undefined;
 
 export default async function addToCartHandler(
   req: NextApiRequest,
-  resp: NextApiResponse<AddToCartResponseBody>,
+  res: NextApiResponse<AddToCartResponseBody>,
 ) {
-  if (req.method !== 'POST') {
-    resp.status(405).json({
-      errors: { message: 'Method not supported, try POST instead' },
+  if (req.method === 'POST') {
+    const accessToken = getCookie('accessToken', { req, res });
+
+    if (accessToken) {
+      const decodedToken = await firebaseAdmin
+        .auth()
+        .verifyIdToken(accessToken);
+
+      // get user
+      const user = await prisma.user.findUnique({
+        where: {
+          user_id_external: decodedToken.uid,
+        },
+        select: {
+          id: true,
+          email: true,
+          username: true,
+        },
+      });
+
+      if (!user) {
+        res.status(401).json({ error: { message: 'Unauthorized' } });
+        return;
+      }
+
+      const itemInCart = req.body;
+
+      // check if product already in cart
+      const cartItems = await prisma.cartItem.findMany({
+        where: { user_id: user.id, product_id: itemInCart.id },
+      });
+
+      if (cartItems.length > 0) {
+        // update quantity
+        // todo: add try{}catch()?
+        const itemUpdatedQuantity = await prisma.cartItem.update({
+          where: {
+            id: cartItems[0].id,
+          },
+          data: {
+            quantity: cartItems[0].quantity + itemInCart.quantity,
+          },
+        });
+
+        if (itemUpdatedQuantity) {
+          return res.status(200).json({
+            id: itemUpdatedQuantity.id,
+            quantity: itemUpdatedQuantity.quantity,
+          });
+        } else
+          return res
+            .status(500)
+            .json({ error: { message: 'Oops, something went wrong ' } });
+      } else {
+        // add new item
+        const newCartItem = await prisma.cartItem.create({
+          data: {
+            product_id: itemInCart.id,
+            quantity: itemInCart.quantity,
+            user_id: user.id,
+          },
+        });
+
+        if (newCartItem) {
+          return res
+            .status(200)
+            .json({ id: newCartItem.id, quantity: newCartItem.quantity });
+        } else {
+          return res
+            .status(500)
+            .json({ error: { message: 'Oops, something went wrong ' } });
+        }
+      }
+    } else {
+      res.status(401).json({
+        error: { message: 'Unauthorized' },
+      });
+    }
+  } else {
+    res.status(405).json({
+      error: { message: 'Method not supported, try POST instead' },
     });
   }
 }
