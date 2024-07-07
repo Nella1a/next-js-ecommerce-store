6f28c4752f3795bf8c08505e30e0788a1d8f2b05
Nella1a <m.allen@gmx.at>
2024-07-07 21:21:13 +0200

    Add logo in new color and import google fonts

---
+++ util/fonts.ts
@@ -0,0 +1,7 @@
+import { Calistoga, Roboto } from 'next/font/google';
+
+export const calistoga = Calistoga({ weight: '400', subsets: ['latin'] });
+export const roboto = Roboto({
+  weight: ['400', '500', '700', '900'],
+  subsets: ['latin'],
+});
